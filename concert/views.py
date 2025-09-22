826603a0e5065e1d69e23e035b404ba5ac80bdff
Sabnei <sebasplay417@gmail.com>
2025-09-22 16:57:34 -0600

    Update signup URL path and implement signup functionality to handle user registration and login.

--- concert/views.py
+++ concert/views.py
@@ -15,7 +15,26 @@ import requests as req
 
 
 def signup(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.filter(username=username).first()
+            if user:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": SignUpForm, "message": "user already exist"},
+                )
+            else:
+                user = User.objects.create(
+                    username=username, password=make_password(password)
+                )
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, "signup.html", {"form": SignUpForm})
+    return render(request, "signup.html", {"form": SignUpForm})
 
 
 def index(request):
